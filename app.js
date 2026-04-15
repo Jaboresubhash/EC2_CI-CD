55939d362ba5962effe28dc7798050ef55a658a5
Jaboresubhash <subhashjabore@gmail.com>
2026-04-15 12:12:41 +0530

    add

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const { Client } = require("pg");
 
+const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express + Node!");
-});
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on EC2 🚀 http://0.0.0.0:${PORT}`);
-});
-
-// ======================
-
-const { Client } = require("pg");
+console.log("DB URL:", process.env.DATABASE_URL);
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
 });
 
 client.connect()
-  .then(() => console.log("Connected to RDS ✅"))
-  .catch(err => console.error("DB Error ❌", err));
+  .then(() => {
+    console.log("Connected to RDS ✅");
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on EC2 🚀 http://0.0.0.0:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("DB Error ❌", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express + Node!");
+});
