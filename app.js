65ad242374ee7934e8a91267ec83bfb7863ed476
Jaboresubhash <subhashjabore@gmail.com>
2026-04-15 11:50:37 +0530

    .env file will add

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Express + Node!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on EC2 🚀 http://0.0.0.0:${PORT}`);
 });
